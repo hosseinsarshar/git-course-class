3ba6c8d4d90f14f8929b48f5deb79f494c24687b
Hossein Sarshar <hossein.sarshar@gmail.com>
2019-10-10 19:22:50 -0400

    Testing the issues #1

--- training.py
+++ training.py
@@ -4,3 +4,12 @@ print('Some random changes 2')
 print('Some random changes 3')
 print('Some random changes 4')
 print('Some random changes 5')
+print('Some random changes 1')
+print('Some random changes 2')
+print('Some random changes 3')
+print('Some random changes 4')
+print('Some random changes 5')
+
+
+print('Testing Issues')
+
