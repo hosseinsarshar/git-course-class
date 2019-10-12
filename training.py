f7bd12a456ccddcaf763daf8762ead8ce8bb3a16
NourFahmy <35409519+NourFahmy@users.noreply.github.com>
2019-10-12 16:01:48 -0400

    Update training.py

--- training.py
+++ training.py
@@ -9,7 +9,7 @@ print('Some random changes 2')
 print('Some random changes 3')
 print('Some random changes 4')
 print('Some random changes 5')
-
+print('Some random changes 6')
 
 print('Testing Issues')
 
